e97f013622929f8442f36d28a827f1aa852de282
Ruben Danielyan <rub1994.13@gmail.com>
2020-04-24 02:29:38 +0400

    Constants module

---
+++ modules/constants/src/constants.ts
@@ -0,0 +1,19 @@
+/*
+* Don't change these config, you will be banned from other nodes
+ */
+
+
+const TRANSACTION_FEE = 1;
+
+const TRANSACTIONS_PER_BLOCK = 3
+
+const VALIDATORS_FEE = 30
+
+const INITIAL_COINS = 10000
+
+export {
+    TRANSACTION_FEE,
+    TRANSACTIONS_PER_BLOCK,
+    VALIDATORS_FEE,
+    INITIAL_COINS,
+};
